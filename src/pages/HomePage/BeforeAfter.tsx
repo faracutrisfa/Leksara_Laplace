ed7e2308f5309d8aa0aab28f925258d524af3d4a
faracutrisfa <faracutrisfa@gmail.com>
2025-11-27 19:27:19 +0700

    feat: add title at some section landing page

--- src/pages/HomePage/BeforeAfter.tsx
+++ src/pages/HomePage/BeforeAfter.tsx
@@ -5,25 +5,20 @@ export default function BeforeAfter() {
     <section className="relative">
       <div className="container">
         <div className="flex justify-center">
-          <div className="inline-flex items-center gap-2 rounded-lg bg-primary-50 px-4 py-1 text-lg font-semibold text-primary-600">
+          <div className="inline-flex items-center gap-2 rounded-lg bg-primary-50 px-4 py-2 text-lg font-semibold text-primary-600">
             <Icon icon="material-symbols:dashboard-rounded" width={18} />
             <span>BeforeAfter</span>
           </div>
         </div>
 
-        <div className="mt-5 flex flex-col items-center text-center md:flex-row md:items-center md:justify-center md:text-left gap-5">
-          <h2 className="text-2xl sm:text-4xl font-semibold text-neutral-900">
-            What is <span className="text-primary-500">Leksara</span>
-          </h2>
+        <h2 className="mt-6 text-center text-2xl sm:text-4xl font-semibold text-neutral-900">
+          <span className="text-primary-500">Before</span> VS After Leksara
+        </h2>
 
-          <div className="hidden md:block h-20 w-1 bg-primary-600" />
-
-          <p className="max-w-4xl text-sm sm:text-lg text-neutral-2 font-semibold">
-            Leksara is a Python toolkit designed to help Data Scientists and
-            Machine Learning Engineers clean and preprocess Indonesian text data
-            efficiently.
-          </p>
-        </div>
+        <p className="mt-4 mx-auto max-w-4xl text-center text-xs md:text-sm lg:text-lg font-semibold text-neutral-600">
+          See how Leksara transforms messy, unstructured text into clean,
+          ready-to-analyze data — instantly.
+        </p>
       </div>
     </section>
   );
--- src/pages/HomePage/Features.tsx
+++ src/pages/HomePage/Features.tsx
@@ -5,25 +5,21 @@ export default function Features() {
     <section className="relative">
       <div className="container">
         <div className="flex justify-center">
-          <div className="inline-flex items-center gap-2 rounded-lg bg-primary-50 px-4 py-1 text-lg font-semibold text-primary-600">
+          <div className="inline-flex items-center gap-2 rounded-lg bg-primary-50 px-4 py-2 text-lg font-semibold text-primary-600">
             <Icon icon="material-symbols:dashboard-rounded" width={18} />
             <span>Features</span>
           </div>
         </div>
 
-        <div className="mt-5 flex flex-col items-center text-center md:flex-row md:items-center md:justify-center md:text-left gap-5">
-          <h2 className="text-2xl sm:text-4xl font-semibold text-neutral-900">
-            What is <span className="text-primary-500">Leksara</span>
-          </h2>
+        <h2 className="mt-6 text-center text-2xl sm:text-4xl font-semibold text-neutral-900">
+          <span className="text-primary-500">Usage</span> Example
+        </h2>
 
-          <div className="hidden md:block h-20 w-1 bg-primary-600" />
-
-          <p className="max-w-4xl text-sm sm:text-lg text-neutral-2 font-semibold">
-            Leksara is a Python toolkit designed to help Data Scientists and
-            Machine Learning Engineers clean and preprocess Indonesian text data
-            efficiently.
-          </p>
-        </div>
+        <p className="mt-4 mx-auto max-w-4xl text-center text-xs md:text-sm lg:text-lg font-semibold text-neutral-600">
+          See how Leksara turns hours of manual text cleaning into a seamless,
+          automated process — so your team can focus on insights, modeling, and
+          innovation.
+        </p>
       </div>
     </section>
   );
